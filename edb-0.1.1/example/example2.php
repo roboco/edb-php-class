877720852c1946b073817d131efb469213029bf7
eddsstudio <eddsstudio@677a2bc6-a438-eff3-2d87-9f17b577142e>
2010-11-16 15:55:26 +0000

    next version developing

---
+++ edb-0.1.1/example/example2.php
@@ -0,0 +1,33 @@
+<?php 
+include('c://temp/edb_google_code/edb-0.1.1/edb-class/edb.class.php');
+
+$db_data = array('divaspuses.db.5696236.hostedresource.com','divaspuses','killersite32A','divaspuses');
+$db_data2 = array('steidzami.db.5696236.hostedresource.com','steidzami','killersite32A','steidzami');
+
+$db = new edb($db_data);
+
+$db2 = new edb($db_data2);
+
+
+
+$one = $db->q("select * from users limit 3");
+
+$one = $db->q("select * from users limit 5,3");
+
+$one2 = $db->q("select * from users limit 9,3");
+
+$one = $db->s("Set names utf8");
+
+$two = $db2->q("select * from users");
+
+
+echo 'time: '.$db->queryTime;
+echo '<br>';
+echo 'count: '.$db->queryCount;
+echo '<br>';
+echo 'time: '.$db2->queryTime;
+
+
+
+print_r($db->queryAll);
+?>
